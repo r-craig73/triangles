b296ff2b869b3c581acbd8cc8ba2722d28080449
Jared Clemmensen and Ron Craig <jared@>
2018-01-11 16:46:28 -0800

    final version

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,25 @@ $(document).ready(function(){
 $("form#triangle").submit(function(event) {
   var side1 = parseInt($("input#length1").val());
   var side2 = parseInt($("input#length2").val());
-  console.log(side2);
   var side3 = parseInt($("input#length3").val());
+
+  if (equilateral(side1, side2, side3)) {
+      $(".result1").text("This is an equilateral triangle");
+    }
+  else if(isosceles(side1, side2, side3)) {
+    $(".result1").text("This is an isosceles triangle");
+    }
+  else if (scalene(side1, side2, side3)) {
+    $(".result1").text("This is an scalene triangle");
+    }
+  else {
+    $(".result1").text("This is not a triangle :-(");
+  }
+
   event.preventDefault();
+
 });
+
 });
 
 function equilateral (s1, s2, s3) {
@@ -26,7 +41,7 @@ function isosceles (s1, s2, s3) {
 };
 
 function scalene (s1, s2, s3) {
-  if (s1 != s2 && s1 != s3 && s2 != s3) {
+  if ((s1 != s2 && s1 != s3 && s2 != s3 ) && (s1 + s2 >= s3 && s2 + s3 >= s1 && s3 + s1 >= s2)) {
     return true;
   } else {
     return false;
@@ -34,7 +49,7 @@ function scalene (s1, s2, s3) {
 };
 
 function nottri (s1, s2, s3) {
-  if (s1 + s2 <= s3 && s2 + s3 <= s1 && s3 + s1 <= s2) {
+  if (s1 + s2 >= s3 && s2 + s3 >= s1 && s3 + s1 >= s2) {
     return true;
   } else {
     return false;
