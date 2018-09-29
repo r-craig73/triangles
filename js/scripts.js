21f3cdd30fc2ea9215cc7878028cefeabc4eac9e
Ron Craig <ron.craig@comcast.net>
2018-09-29 13:52:19 -0700

    remove whitespece and update indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,23 @@
 $(document).ready(function(){
+  $("form#triangle").submit(function(event) {
+    var side1 = parseInt($("input#length1").val());
+    var side2 = parseInt($("input#length2").val());
+    var side3 = parseInt($("input#length3").val());
 
-$("form#triangle").submit(function(event) {
-  var side1 = parseInt($("input#length1").val());
-  var side2 = parseInt($("input#length2").val());
-  var side3 = parseInt($("input#length3").val());
-
-  if (equilateral(side1, side2, side3)) {
-      $(".result1").text("This is an equilateral triangle");
+    if (equilateral(side1, side2, side3)) {
+        $(".result1").text("This is an equilateral triangle");
+      }
+    else if(isosceles(side1, side2, side3)) {
+      $(".result1").text("This is an isosceles triangle");
+      }
+    else if (scalene(side1, side2, side3)) {
+      $(".result1").text("This is an scalene triangle");
+      }
+    else {
+      $(".result1").text("This is not a triangle :-(");
     }
-  else if(isosceles(side1, side2, side3)) {
-    $(".result1").text("This is an isosceles triangle");
-    }
-  else if (scalene(side1, side2, side3)) {
-    $(".result1").text("This is an scalene triangle");
-    }
-  else {
-    $(".result1").text("This is not a triangle :-(");
-  }
-
-  event.preventDefault();
-
-});
-
+    event.preventDefault();
+  });
 });
 
 function equilateral (s1, s2, s3) {
